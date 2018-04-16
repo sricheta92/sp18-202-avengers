d5ee494b327d30ba227b230297aa6a8246eae094
Huy Vo <huyvanvo94@gmail.com>
2018-04-16 02:07:39 -0700

    add nodemon for testing

---
+++ 2d-game-app/README.md
@@ -0,0 +1,5 @@
+
+** Application uses nodemon for test development **
+** To run in test development type: **
+
+`npm run test`
--- 2d-game-app/config.js
+++ 2d-game-app/config.js
@@ -1,6 +1,6 @@
 const config = {
   host: process.env.IP || '0.0.0.0',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3000
 };
 
 
