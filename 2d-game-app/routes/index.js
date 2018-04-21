cef9d9c7d2bb10c863733829e3aa8826aeefb938
Huy Vo <huyvanvo94@gmail.com>
2018-04-21 02:38:00 -0700

    init index.html

--- 2d-game-app/routes/index.js
+++ 2d-game-app/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('public/index.html')
+  console.log("get index.html");
+  res.sendFile('public/index.html');
 });
 
 module.exports = router;
