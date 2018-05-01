ef319723a312e342574f792dad6ba6ac68d79526
MangeshTak <32856521+MangeshTak@users.noreply.github.com>
2018-05-01 15:00:31 -0700

    Correcting Proxy

--- js-game/public/js/Game.js
+++ js-game/public/js/Game.js
@@ -1,17 +1,51 @@
-var game = new Phaser.Game(640	,640 , Phaser.CANVAS , '');
-
+/*var game = new Phaser.Game(640	,640 , Phaser.CANVAS , '');
+var authenticated = false;
 game.state.add('Boot', Game.Boot);
 game.state.add('Preloader', Game.Preloader);
 game.state.add('MainMenu', Game.MainMenu);
 // Load levels
-
 game.state.add('Level1', Game.Level1);
 game.state.add('FairyLevel', Game.FairyLevel);
-
+*/
 
 /**
  * Start boot
  */
-game.onStart = function () {
+/*game.onStart = function () {
     game.state.start('Boot');
-};
+};*/
+
+function GameProxy() {
+
+	this.game = new Phaser.Game(640	,640 , Phaser.CANVAS , '');
+
+	this.authenticated = true;
+
+	this.game.state.add('Boot', Game.Boot);
+	this.game.state.add('Preloader', Game.Preloader);
+	this.game.state.add('MainMenu', Game.MainMenu);
+	// Load levels
+
+	this.game.state.add('Level1', Game.Level1);
+	this.game.state.add('FairyLevel', Game.FairyLevel);
+
+	 
+}
+
+GameProxy.prototype.onStart = function () {
+
+	 if(this.authenticated===true)
+	 {
+	 	this.game.state.start('Boot');	
+	 }
+	 else
+	 {
+	 	// to-do
+	 }
+
+
+	
+}
+
+var game = new GameProxy();
+game.onStart();
