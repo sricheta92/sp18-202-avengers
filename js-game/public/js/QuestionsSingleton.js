4c2ff0d3a7e19f00f8a05037bc846361c0a1f126
Huy Vo <huyvanvo94@gmail.com>
2018-05-02 07:42:40 -0700

    Update QuestionsSingleton.js

--- js-game/public/js/QuestionsSingleton.js
+++ js-game/public/js/QuestionsSingleton.js
@@ -21,23 +21,22 @@ function QuestionsSingleton(){
   instance.constructor = QuestionsSingleton; 
  
   // function
-  instance.get = function (idx) {
-    function(idx){
+  instance.get = function (idx) { 
     if(idx === 0){
-        return ['What is the design pattern that is used when creation of object directly is costly?',  'prototype']
+        return ['What is the design pattern that is used when creation of object directly is costly?',  'prototype'];
     }
     
     if(idx === 1){
-        return ['Define a family of algorithms, encapsulate each one, and make them interchangeable. It lets the algorithm vary independently from clients that use it.', 'strategy']
+        return ['Define a family of algorithms, encapsulate each one, and make them interchangeable. It lets the algorithm vary independently from clients that use it.', 'strategy'];
     }
     
     if(idx === 2){
-        return ['Encapsulate a request as an object, there by letting you parametrize clients with different requests, queue or log requests, and support undoable operation.', 'command']
+        return ['Encapsulate a request as an object, there by letting you parametrize clients with different requests, queue or log requests, and support undoable operation.', 'command'];
      
     }
     
     
-    return ['Provides a way to access the elements of an aggregate object sequentially without exposing its underlying representation.', 'iterator']
+    return ['Provides a way to access the elements of an aggregate object sequentially without exposing its underlying representation.', 'iterator'];
     
   };
 
