4a6e91a1c9539184908c14efc7b67c2493edcaa8
Huy Vo <huyvanvo94@gmail.com>
2018-05-04 22:00:29 -0700

    update command pattern

--- js-game/public/js/Command.js
+++ js-game/public/js/Command.js
@@ -2,13 +2,13 @@
  * @author: Huy Vo 
  * @description: Command Design Pattern 
  */
-function Command(context, execute) {
+function Command(state, execute) {
     this.execute = execute;
-    this.context = context;
+    this.state = state; 
 
 }
 
 
 Command.prototype.update = function () {
-    this.execute(this.context);
+    this.execute(this.state);
 };
